0c8038f8b89998b9ee23504875ab5e69c970d5c6
gbrousse-recia <gregory.brousse@recia.fr>
2022-06-02 14:16:42 +0200

    Update : GIP adress

--- themes/yaktheme_reciaforms/templates/footer-gip.tpl.php
+++ themes/yaktheme_reciaforms/templates/footer-gip.tpl.php
@@ -3,7 +3,7 @@
         <div class="recia-footer-coord">
             <?= $image ?>
             <p class="footer-contact mt-3 hidden-pod">
-                <b>GIP RECIA</b> &bull; Région Centre inter Active<br>
+                <b>GIP RECIA &bull; Région Centre inter Active</b><br>
                 3 avenue Claude-Guillemin &bull; Bâtiment F1<br>
                 BP 36009 &bull; 45060 ORLÉANS Cedex 2<br>
                 <a href="https://www.recia.fr" target="_blank">www.recia.fr</a><br>
